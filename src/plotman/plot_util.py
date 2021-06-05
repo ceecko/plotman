646c7b0e2e6745f2ee652ec54be9a88ad1ada718
ceecko <ceecko@gmail.com>
2021-06-05 15:47:06 +0200

    Keep relative path in rsync

--- src/plotman/plot_util.py
+++ src/plotman/plot_util.py
@@ -51,7 +51,9 @@ def list_k32_plots(d):
     for root, dirs, files in os.walk(d):
         for plot in files:
             if re.match(r'^plot-k32-.*plot$', plot):
-                plot = os.path.join(root, plot)
+                # This ensures the relative path to plot is joined by /./
+                # Rsync uses it to keep directory structure via -R parameter
+                plot = os.path.join(d+'/', '.', root[len(d)+1:], 'file.plot')
                 try:
                     if os.stat(plot).st_size > (0.95 * get_k32_plotsize()):
                         plots.append(plot)
